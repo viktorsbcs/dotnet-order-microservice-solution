29f7af26cd3221a958c7d9ca84440feba5dfe088
Viktors Bcs <56092407+viktorsbcs@users.noreply.github.com>
2024-07-14 04:39:36 +0300

    removing unnecessary order controller endpoints

--- Order.API/Order.API/Controllers/OrderController.cs
+++ Order.API/Order.API/Controllers/OrderController.cs
@@ -13,35 +13,8 @@ namespace Order.API.Controllers
 
         }
 
-        // GET: api/<MatchController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<MatchController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<MatchController>
         [HttpPost]
         public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<MatchController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<MatchController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
         {
         }
     }
