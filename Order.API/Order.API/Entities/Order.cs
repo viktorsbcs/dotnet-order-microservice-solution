615eba4decf5caea7e6febd81f9bd0a8b972bee8
Viktors Bcs <56092407+viktorsbcs@users.noreply.github.com>
2024-07-14 04:39:10 +0300

    order entity

---
+++ Order.API/Order.API/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace Order.API.Entities
+{
+    public class Order
+    {
+        public double Price { get; set; }
+        public Guid Id { get; set; }
+        public string? PartName { get; set; }
+        public PartType PartType { get; set; }
+
+    }
+
+    public enum PartType
+    {
+        Engine = 0,
+        Suspension = 1,
+        ExhaustSystem = 2,
+        FuelSystem = 3
+    }
+}
