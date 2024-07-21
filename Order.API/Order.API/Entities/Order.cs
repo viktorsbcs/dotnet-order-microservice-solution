182a44c6fa3ecdd7508719de40d24c0c53257766
Viktors Bcs <56092407+viktorsbcs@users.noreply.github.com>
2024-07-21 04:50:17 +0300

    simple order class

--- Order.API/Order.API/Entities/Order.cs
+++ Order.API/Order.API/Entities/Order.cs
@@ -2,18 +2,7 @@
 {
     public class Order
     {
-        public double Price { get; set; }
         public Guid Id { get; set; }
-        public string? PartName { get; set; }
-        public PartType PartType { get; set; }
-
     }
 
-    public enum PartType
-    {
-        Engine = 0,
-        Suspension = 1,
-        ExhaustSystem = 2,
-        FuelSystem = 3
-    }
 }
