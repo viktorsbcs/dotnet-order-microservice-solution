a4d19214f3c463c87cdfc5ec65e575a410cc5827
Viktors Bcs <56092407+viktorsbcs@users.noreply.github.com>
2024-07-29 02:13:29 +0300

    Create Order DbContext

---
+++ Order.API/Order.API/DatabaseContexts/OrderContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.API.Entities;
+
+namespace Order.API.DatabaseContexts
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<UserOrder> UserOrders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}
