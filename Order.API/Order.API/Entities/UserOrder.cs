d40cdb3abd5b1aceeeebda4d10d60894227205ba
Viktors Bcs <56092407+viktorsbcs@users.noreply.github.com>
2024-08-04 14:28:13 +0300

    Add new field in UserOrder entity model

--- Order.API/Order.API/Entities/UserOrder.cs
+++ Order.API/Order.API/Entities/UserOrder.cs
@@ -1,8 +1,15 @@
-﻿namespace Order.API.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Order.API.Entities
 {
+    [Table("UserOrders", Schema = "dbo")]
     public class UserOrder
     {
+        [Required]
         public Guid Id { get; set; }
-    }
 
+        [Required]
+        public DateTime OrderDate { get; set; }
+    }
 }
