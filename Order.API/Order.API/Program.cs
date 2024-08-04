af0090872b5d039e8889918a06b0781d759f09c1
Viktors Bcs <56092407+viktorsbcs@users.noreply.github.com>
2024-08-05 00:04:59 +0300

    Replace connection string credentials from user secrets

--- Order.API/Order.API/Program.cs
+++ Order.API/Order.API/Program.cs
@@ -2,6 +2,8 @@ using Order.API.DatabaseContexts;
 using Npgsql;
 using Microsoft.EntityFrameworkCore;
 using Order.API.Repositories;
+using System.Text;
+using Microsoft.Extensions.Configuration;
 
 internal class Program
 {
@@ -16,11 +18,19 @@ internal class Program
         builder.Services.AddScoped<DbContext, OrderContext>();
         builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
+        var connectionStringBuilder = new StringBuilder(builder.Configuration.GetConnectionString("OrderContext"));
+
+        if (builder.Environment.IsDevelopment())
+        {
+            connectionStringBuilder.Replace("{{postgres_user}}", builder.Configuration["postgres_user"]);
+            connectionStringBuilder.Replace("{{postgres_pw}}", builder.Configuration["postgres_pw"]);
+        }
+
         builder.Services.AddDbContext<OrderContext>(options =>
         {
-            options.UseNpgsql(builder.Configuration.GetConnectionString("OrderContext"));
+            options.UseNpgsql(connectionStringBuilder.ToString());
         });
-        
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
