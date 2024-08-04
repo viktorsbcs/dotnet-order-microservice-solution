31a007b3073f72b631020cea1e33f36adda7725a
Viktors Bcs <56092407+viktorsbcs@users.noreply.github.com>
2024-08-04 14:28:29 +0300

    Inject additional dependencies in OrderContext

--- Order.API/Order.API/DatabaseContexts/OrderContext.cs
+++ Order.API/Order.API/DatabaseContexts/OrderContext.cs
@@ -5,16 +5,20 @@ namespace Order.API.DatabaseContexts
 {
     public class OrderContext : DbContext
     {
-        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        private readonly DbContextOptions<OrderContext> _options;
+        private readonly ILogger<OrderContext> _logger;
+
+        public OrderContext(DbContextOptions<OrderContext> options, ILogger<OrderContext> logger) : base(options)
         {
-            
+            _options = options;
+            _logger = logger;
         }
 
         public DbSet<UserOrder> UserOrders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            
         }
 
     }
