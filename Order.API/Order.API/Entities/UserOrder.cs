5925dd80eefcdb011730bf072dd33a4fc21b8f25
Viktors Bcs <56092407+viktorsbcs@users.noreply.github.com>
2024-08-04 22:48:14 +0300

    UserOrder model column and table name adjustments

--- Order.API/Order.API/Entities/UserOrder.cs
+++ Order.API/Order.API/Entities/UserOrder.cs
@@ -3,13 +3,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Order.API.Entities
 {
-    [Table("UserOrders", Schema = "dbo")]
+    [Table("userorders", Schema = "dbo")]
     public class UserOrder
     {
         [Required]
+        [Column("id")]
         public Guid Id { get; set; }
 
         [Required]
+        [Column("orderdate")]
         public DateTime OrderDate { get; set; }
     }
 }
