e537465f63c5c403e55d5b5d246c6a1f349ab262
Viktors Bcs <56092407+viktorsbcs@users.noreply.github.com>
2024-08-19 01:23:25 +0300

    express api adding cors package

--- OrderGui.API/app.js
+++ OrderGui.API/app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors')
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
 });
 
-app.get("/status", (request, response) => {
-    const status = {
-        "Status": "Running"
-    };
+app.get("/orders", cors(), (request, response) => {
 
-    response.send(status);
+    const orders = [
+        { id: 1, value: "aaa"},
+        { id: 2, value: "bbb"},
+        { id: 3, value: "ccc"},
+        { id: 4, value: "ddd"},
+    ]
+
+    
+
+    response.send(orders);
 });
